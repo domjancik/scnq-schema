e7d0126bda648d3b2f65931484e7603834422076
Dominik Jančík <hi@domj.net>
2019-12-22 15:55:53 +0100

    FileUtils C# based DLL

---
+++ Main/lib/FileUtils/FileUtils/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Schema
+{
+    // 1st C# DLL for Schéma
+    public static class FileUtils
+    {
+        public static string SanitizeFileName(string input)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+            string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return System.Text.RegularExpressions.Regex.Replace(input, invalidRegStr, "_");
+        }
+
+        public static string SanitizePath(string input)
+        {
+            string invalidChars = System.Text.RegularExpressions.Regex.Escape(@"\/:*?<>|");
+            string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return System.Text.RegularExpressions.Regex.Replace(input, invalidRegStr, "_");
+        }
+    }
+}
