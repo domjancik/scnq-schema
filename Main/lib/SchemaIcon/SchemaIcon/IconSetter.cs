17806f7fcd796b7792fa5702181b43859b1d5f57
Dominik Jančík <hi@domj.net>
2020-01-03 10:20:31 +0100

    Schema .ico and a library to set it at runtime
    SchemaIcon.dll + its VS project

---
+++ Main/lib/SchemaIcon/SchemaIcon/IconSetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchemaIcon
+{
+    public class IconSetter
+    {
+        public static Form SetIcon(Form form)
+        {
+            form.Icon = Properties.Resources.SchemaLogo2_01;
+            return form;
+        }
+    }
+}
