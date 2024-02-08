a24d8b5c86716a5594a5d1c62f5deb2b995adf0f
Dominik Jancik <hi@domj.net>
2024-02-08 22:15:05 +0100

    feat: YAMLStacks plugin

---
+++ Main/vl/Plugins/YAMLStacks/csharp/Schema.FileFormats.YAMLStacks/YAMLStack.cs
@@ -0,0 +1,12 @@
+namespace Schema.FileFormats.YAMLStacks;
+
+public class YAMLStack
+{
+    
+    public string name = "";
+    public object stack = null;
+    
+    public string? author;
+    public string? date;
+    public string[]? dependencies;
+}
